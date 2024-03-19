990adb6cbedca56006e12c4eb569201e1799fc47
jahandar <axiyev.cahandar@gmail.com>
2024-03-19 01:36:50 +0100

    commit

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -62,6 +62,18 @@ export default function Sidebar() {
             <img src="./assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
             <span className="sidebarFriendName">Jane Doe</span>
           </li>
+          <li className="sidebarFriend">
+            <img src="./assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+            <span className="sidebarFriendName">Jane Doe</span>
+          </li>
+          <li className="sidebarFriend">
+            <img src="./assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+            <span className="sidebarFriendName">Jane Doe</span>
+          </li>
+          <li className="sidebarFriend">
+            <img src="./assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+            <span className="sidebarFriendName">Jane Doe</span>
+          </li>
         </ul>
       </div>
     </div>
